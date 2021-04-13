ec536cea4d44f8a540a56782a6a98a754543cd29
snct-ukai <73654679+snct-ukai@users.noreply.github.com>
2021-04-13 17:08:31 +0900

    Update report1-2.cpp

--- report1-2.cpp
+++ report1-2.cpp
@@ -3,8 +3,7 @@ using namespace std;
 
 int gcd(int m,int n){
     while(m*n){
-       int t=((m>n)?m:n)%((m<n)?m:n);
-       ((m>n)?m:n)=t; 
+       ((m>n)?m:n)=((m>n)?m:n)%((m<n)?m:n);
     }
     return ((m>n)?m:n);
 }
@@ -16,8 +15,6 @@ int lcm(int m,int n){
 int main(void){
     int l,m,n;
     cin>>l>>m>>n;
-    int k=gcd(l,m);
-    cout<<gcd(k,n)<<endl;
-    int o=lcm(l,m);
-    cout<<lcm(n,o)<<endl;
+    cout<<gcd(gcd(l,m),n)<<endl;
+    cout<<lcm(n,lcm(l,m))<<endl;
 }
